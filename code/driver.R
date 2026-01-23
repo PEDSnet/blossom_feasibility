9d18512204acc6b59f01deff5ab6026e15da40f9
hirabayask <hirabayask@chop.edu>
2026-01-23 12:25:06 -0500

    add notes on phoenix8 data availability

--- code/driver.R
+++ code/driver.R
@@ -275,10 +275,115 @@ output_tbl(attrition_tbl2, 'attrition2')
 
 ################################################################################
 
+#' Take a look at concepts composing the Phoenix-8 Sepsis score components
+
+######################################################
+#' Components in pSOFA not shown here; work has been conducted for the TRIP study
+
+######################################################
+#' Mechanical ventilation: Previous codeset exists
+mv_codes <- load_codeset('px_2021_11_MechanicalVentilation_V1')
+
+mv1 <- cdm_tbl('procedure_occurrence') %>% inner_join(mv_codes %>% select(concept_id), by=c('procedure_concept_id'='concept_id'))
+mv2 <- cdm_tbl('condition_occurrence') %>% inner_join(mv_codes %>% select(concept_id), by=c('condition_concept_id'='concept_id'))
+mv1 %>% group_by(site) %>% summarise(n=n())
+
+######################################################
+#' D-dimer: Previous codeset exists
+dd_codes <- load_codeset('ddimer_labs')
+
+dd1 <- cdm_tbl('measurement_labs') %>% inner_join(dd_codes %>% select(concept_id), by=c('measurement_concept_id'='concept_id'))
+dd1 %>% group_by(site) %>% summarise(n=n())
+
+######################################################
+#' ANC (absolute neutrophil count): Previous codeset exists
+anc_codes <- load_codeset('anc_labs')
+
+anc1 <- cdm_tbl('measurement_labs') %>% inner_join(anc_codes %>% select(concept_id), by=c('measurement_concept_id'='concept_id'))
+anc1 %>% group_by(site) %>% summarise(n=n())
+
+######################################################
+#' ALT (alanine aminotransferase blood test): Previous codeset exists
+alt_codes <- load_codeset('labs_alt')
+
+alt1 <- cdm_tbl('measurement_labs') %>% inner_join(alt_codes %>% select(concept_id), by=c('measurement_concept_id'='concept_id'))
+alt1 %>% group_by(site) %>% summarise(n=n())
 
 
+######################################################
+#' Generating preliminary review of concepts
+######################################################
+#' International normalized ratio
+inr <- vocabulary_tbl('concept') %>% filter(regexp_like(tolower(concept_name),'international')) %>%
+  filter(regexp_like(tolower(concept_name),'normalized')) %>%
+  filter(regexp_like(tolower(concept_name),'ratio'))
 
+inr1 <- cdm_tbl('measurement_labs') %>% inner_join(inr %>% distinct(concept_id), by=c('measurement_concept_id'='concept_id'))
+#inr2 <- cdm_tbl('measurement_vitals') %>% inner_join(inr %>% distinct(concept_id), by=c('measurement_concept_id'='concept_id'))
+#inr3 <- cdm_tbl('procedure_occurrence') %>% inner_join(inr %>% distinct(concept_id), by=c('procedure_concept_id'='concept_id'))
+#inr4 <- cdm_tbl('observation') %>% inner_join(inr %>% distinct(concept_id), by=c('observation_concept_id'='concept_id'))
+#inr5 <- cdm_tbl('condition_occurrence') %>% inner_join(inr %>% distinct(concept_id), by=c('condition_concept_id'='concept_id'))
+inr1 %>% group_by(site) %>% summarise(n=n())
 
+######################################################
+#' Pupillary exam
+pup <- vocabulary_tbl('concept') %>% filter(regexp_like(tolower(concept_name),'pupillary|pupil|mydriasis')) %>%
+  filter(regexp_like(tolower(concept_name),'reactive|fixed'))
+  #filter(regexp_like(tolower(concept_name),'exam'))
+
+pup1 <- cdm_tbl('condition_occurrence') %>% inner_join(pup %>% distinct(concept_id), by=c('condition_concept_id'='concept_id'))
+#pup2 <- cdm_tbl('observation') %>% inner_join(pup %>% distinct(concept_id), by=c('observation_concept_id'='concept_id'))
+pup1 %>% group_by(site) %>% summarise(n=n())
+
+######################################################
+#' Serum lactate
+
+sl <- vocabulary_tbl('concept') %>% filter(regexp_like(tolower(concept_name),'serum')) %>%
+  filter(regexp_like(tolower(concept_name),'lactate')) %>%
+  filter(!regexp_like(tolower(concept_name),'dehydrogenase')) 
+
+sl1 <- cdm_tbl('measurement_labs') %>% inner_join(sl %>% distinct(concept_id), by=c('measurement_concept_id'='concept_id'))
+sl1 %>% group_by(site) %>% summarise(n=n())
+sl_codes2 <- sl1 %>% distinct(measurement_concept_id) %>% left_join(sl, by=c('measurement_concept_id'='concept_id'))
+
+######################################################
+#' Serum fibrinogen
+
+sf <- vocabulary_tbl('concept') %>% filter(regexp_like(tolower(concept_name),'serum')) %>%
+  filter(regexp_like(tolower(concept_name),'fibrinogen'))
+
+sf1 <- cdm_tbl('measurement_labs') %>% inner_join(sl %>% distinct(concept_id), by=c('measurement_concept_id'='concept_id'))
+sf1 %>% group_by(site) %>% summarise(n=n())
+sf_codes2 <- sf1 %>% distinct(measurement_concept_id) %>% left_join(sf, by=c('measurement_concept_id'='concept_id'))
+
+######################################################
+#' Blood glucose
+
+bg <- vocabulary_tbl('concept') %>% filter(regexp_like(tolower(concept_name),'blood')) %>%
+  filter(regexp_like(tolower(concept_name),'glucose'))
+
+bg1 <- cdm_tbl('measurement_labs') %>% inner_join(bg %>% distinct(concept_id), by=c('measurement_concept_id'='concept_id'))
+bg1 %>% group_by(site) %>% summarise(n=n())
+bg_codes2 <- bg1 %>% distinct(measurement_concept_id) %>% left_join(bg, by=c('measurement_concept_id'='concept_id'))
+
+######################################################
+#' ALC (absolute lymphocyte count)
+
+#' I think these codes are perhaps too expansive
+alc <- vocabulary_tbl('concept') %>%
+  #filter(regexp_like(tolower(concept_name),'absolute')) %>%
+  filter(regexp_like(tolower(concept_name),'lymphocyte')) #%>%
+  #filter(regexp_like(tolower(concept_name),'count'))
+
+alc1 <- cdm_tbl('measurement_labs') %>% inner_join(alc %>% distinct(concept_id), by=c('measurement_concept_id'='concept_id'))
+alc1 %>% group_by(site) %>% summarise(n=n())
+alc_codes2 <- alc1 %>% distinct(measurement_concept_id) %>% left_join(alc, by=c('measurement_concept_id'='concept_id'))
+
+######################################################
+#' Add my CSV with Phoenix-8 Sepsis Score component notes
+
+phoenix8_notes <- read_csv('specs/phoenix8_components.csv')
+output_tbl(phoenix8_notes, 'phoenix8_notes')
 
 
 
